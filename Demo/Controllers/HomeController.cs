21de0e3604d9e653a7e77c046a6bf0a7eec68320
avgprog777 <shindesopan420@gmail.com>
2026-05-01 23:19:22 +0530

    add comment on home controller from branch feature-2-add

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace Demo.Controllers
 {
     public class HomeController : Controller
     {
+        //add
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
